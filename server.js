6dd928147a38581a3d8c5d663f21225e569e1dd8
JacK9004 <kjushkin@gmail.com>
2024-02-06 00:33:24 +0900

    fix: harid.ejs and server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // 2 Session code
 // 3 Views code
@@ -14,12 +14,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: red">HELLO WORLD by JacK</h1>`);
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
 });
 
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
