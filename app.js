3c5a83ff2b5943c812a64a9f8372e46b54595a3a
JacK9004 <kjushkin@gmail.com>
2024-02-12 09:30:12 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -28,16 +28,37 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
+    console.log('user entered / create-item');
     console.log(req.body);
-    res.json({test: "success"});
+    const new_reja = req.body.reja;
+    db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wromg");
+        } else {
+            res.end("Successfully added");
+        }
+    });
+    
 });
 
-app.get('/author', (req, res) => {
-    res.render("author", {user: user});
-});
+// app.get('/author', (req, res) => {
+//     res.render("author", {user: user});
+// });
 
 app.get("/", function (req, res) {
-    res.render("reja");
+    console.log('user entered /');
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            res.render("reja", { items: data });
+        }
+    });
+    
 });
 
 module.exports = app;
